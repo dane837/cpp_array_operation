68838a5dabdfdac148d7e3627a510e34e4a5a580
Leng Dane <lengdane1999@gmail.com>
2021-07-08 22:06:28 +0700

    Updated on Union

--- cpp_array/main.cpp
+++ cpp_array/main.cpp
@@ -4,6 +4,26 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int A[5] = {1,2,3,5,6};
+    int B[8] = {1,5,6,8,5,3,6,3};
+
+    int ASize = sizeof (A)/sizeof (int);
+    int BSize = sizeof (B)/sizeof (int);
+
+    // Union
+    int Union[ASize + BSize];
+
+    for(int i = 0 ; i < ASize ; i++){
+        Union[i] = A[i];
+    }
+    for(int i = ASize, j = 0 ; i < (ASize + BSize) - 1 , j < BSize ; i++, j++){
+        Union[i] = B[j];
+    }
+    cout<<"Union of A and B: ";
+    for(int i = 0 ; i < ASize + BSize; i++){
+        cout<<Union[i];
+    }
+
+    // Intersect
     return 0;
 }
