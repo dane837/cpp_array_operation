199feaf4e04da7d7c6980c4da3b9b951dda2c251
Leng Dane <lengdane1999@gmail.com>
2021-07-11 23:44:12 +0700

    Completed of Intersect

--- cpp_array/union.h
+++ cpp_array/union.h
@@ -1,11 +1,11 @@
+#ifndef UNION_H
+#define UNION_H
 #include <iostream>
 
 using namespace std;
-
-int main(){
-
+int Union(){
     int A[4] = {1,2,3,5};
-    int B[8] = {1,5,6,8,5,3,6,8};
+    int B[8] = {1,5,6,8,5,3,6,3};
 
 
     int ASize = sizeof (A)/sizeof (int);
@@ -19,27 +19,9 @@ int main(){
     for(int i = ASize, j = 0 ; i < (ASize + BSize) - 1, j < BSize ; i++, j++){
         Union[i] = B[j];
     }
-    cout<<"Union Of A and B: ";
+    cout<<"Union Of A and B"<<endl;
     for(int i = 0; i<ASize + BSize; i++){
         cout<<Union[i];
-    }
-     //Intersect
-
-    cout<<" Intersection Of A and B: "<<endl;
-    for (int i = 0; i < ASize; i++)
-    {
-        for (int j = 0; j < BSize; j++)
-        {
-            if (A[i] == B[j])
-            {
-                cout << A[i] << " ";
-            }
-        }
-    }
-
-
-//     A Except B
-
-//     B Except A
-    return 0;
 }
+
+#endif // UNION_H
