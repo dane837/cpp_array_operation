f32317348268d7f7890226c1f5a91ce8a88c8c36
Leng Dane <lengdane1999@gmail.com>
2021-07-15 15:43:48 +0700

    Completed

--- main.cpp
+++ main.cpp
@@ -4,44 +4,108 @@ using namespace std;
 
 int main(){
 
-    int A[4] = {1,2,3,5};
-    int B[8] = {1,5,6,8,5,3,6,8};
+
+    int A[7] = {2,3,4,5,6,7,8};
+    int B[4] = {2,5,7,9};
 
 
     int ASize = sizeof (A)/sizeof (int);
     int BSize = sizeof(B)/sizeof (int);
 
-   //union
-    int Union [ASize + BSize];
-    for (int i =0; i<ASize; i++){
-        Union[i] = A[i];
+    cout<<"A = { ";
+    for (int i = 0; i<ASize; i++){
+        cout<<A[i]<<" ";
     }
-    for(int i = ASize, j = 0 ; i < (ASize + BSize) - 1, j < BSize ; i++, j++){
-        Union[i] = B[j];
-    }
-    cout<<"Union Of A and B: ";
-    for(int i = 0; i<ASize + BSize; i++){
-        cout<<Union[i];
-    }
-    cout<<""<<endl;
-     //Intersect
+    cout<<"}"<<endl;
 
-    cout<<"Intersection Of A and B: "<<endl;
+    cout<<"B = { ";
+    for (int j = 0; j<BSize; j++){
+        cout<<B[j]<<" ";
+    }
+    cout<<"}"<<endl;
+
+   //union
+
+    cout<<"Union Of A and B: ";
+
+
+    int i=0, j=0;
+    for ( ; (i<ASize) && (j<BSize) ; ){
+        if ( A[i] < B[j] ){
+            cout<< A[i++] <<" ";
+        }
+        else if( B[j] < A[i] ){
+            cout<< B[j++] << " ";
+        }
+        else{
+            cout<< B[j++] << " ";
+            i++;
+        }
+    }
+
+
+    for ( ; i<ASize ;i++ ){
+        cout<< A[i]<<" ";
+    }
+    for ( ; j<BSize ;j++ ){
+        cout<< B[j]<<" ";
+    }
+
+    cout<<endl;
+
+    //Intersection
+
+    cout<<"Intersection Of A and B: ";
+
     for (int i = 0; i < ASize; i++)
     {
         for (int j = 0; j < BSize; j++)
         {
             if (A[i] == B[j])
             {
-                cout << A[i] << " ";
+                cout << A[i]<<" ";
             }
         }
     }
+    cout<<""<<endl;
 
 
 //     A Except B
-    
+    cout<<"A Except B: ";
+    for (int i = 0; i < ASize; i++)
+    {
+        bool run = false;
+        for (int j = 0; j<BSize; j++){
+            if (A[i] == B[j]){
+                run = true;
+            }
+        }
+
+        if(!run){
+            cout<<A[i]<<" ";
+        }
+
+    }
+    cout<<""<<endl;
+
 
 //     B Except A
+    cout<<"B Except A: ";
+    for (int j = 0; j < BSize; j++)
+    {
+        bool run = false;
+        for (int i = 0; i<ASize; i++){
+            if (B[j] == A[i]){
+                run = true;
+            }
+        }
+
+        if(!run){
+            cout<<B[j]<<" ";
+        }
+
+    }
+    cout<<""<<endl;
+
     return 0;
 }
